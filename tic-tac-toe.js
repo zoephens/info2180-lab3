f47f10e925983960b81e2656a127cf72bce089a4
Zoe Stephens <zoe01stephens@gmail.com>
2022-10-07 13:09:17 -0500

    Added Exercise 2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,8 +1,36 @@
 window.onload = function() {
+    //Exercise 1
     let container = document.getElementById("board");
     let box = container.getElementsByTagName("div");
-    for (let i = 0; i <=9; i++) {
-        each = box[i];
+    let arr = [1,2,3,4,5,6,7,8,9];
+    let turn = true;
+
+    
+    //iterating through list of divs
+    for (let i = 0; i < 9; i++) {
+        let each = box[i];
         each.classList.add("square");
+        
+        //Exercise 2
+
+        each.addEventListener("click", function(){
+            //(turn === true) ? each.textContent = "X"  :each.textContent = "O";
+            if (turn == true) {
+                each.textContent = "X";
+                turn = false;
+                each.classList.add("X");
+            }
+            else{
+                each.textContent = "O";
+                turn = true;
+                each.classList.add("O");
+            }
+        })
+
     }
+
+    
+    
+
   };
+
